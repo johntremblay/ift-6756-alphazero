bb48a22a7a249674be8ab008c360eda404c81f7d
JOHN <jonathan.tremblay.11@gmail.com>
2021-04-05 13:00:43 -0400

    new rep

--- check_a_s.py
+++ check_a_s.py
@@ -1,3 +1,12 @@
-from .santorini.SantoriniGame import SantoriniGame
+from santorini.SantoriniGame import SantoriniGame
 
-g = SantoriniGame
+g = SantoriniGame(5,4)
+
+import json
+
+dict = g.get_all_action_move()
+
+json = json.dumps(dict)
+f = open("dict.json","w")
+f.write(json)
+f.close()
