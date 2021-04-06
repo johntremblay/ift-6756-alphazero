74de9d77220356feb4463596feffbd7a5e76626d
JOHN <jonathan.tremblay.11@gmail.com>
2021-04-05 22:43:34 -0400

    added res

--- santorini/pytorch/SantoriniNNet.py
+++ santorini/pytorch/SantoriniNNet.py
@@ -22,8 +22,9 @@ class SantoriniNNet(nn.Module):
         self.conv2 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
         self.conv3 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
         self.conv4 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
-        self.conv5 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1)
+        self.conv5 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
         self.conv6 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1)
+        self.conv7 = nn.Conv3d(args.num_channels, args.num_channels, 3, stride=1)
 
 
         self.bn1 = nn.BatchNorm3d(args.num_channels)
@@ -32,6 +33,7 @@ class SantoriniNNet(nn.Module):
         self.bn4 = nn.BatchNorm3d(args.num_channels)
         self.bn5 = nn.BatchNorm3d(args.num_channels)
         self.bn6 = nn.BatchNorm3d(args.num_channels)
+        self.bn7 = nn.BatchNorm3d(args.num_channels)
 
 
         self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4), 13*1024)
@@ -46,12 +48,15 @@ class SantoriniNNet(nn.Module):
 
     def forward(self, s):
         s = s.view(-1, 1, self.board_x, self.board_y, self.board_z)
+        res = s
         s = F.relu(self.bn1(self.conv1(s)))
         s = F.relu(self.bn2(self.conv2(s)))
+        s = res + s
         s = F.relu(self.bn3(self.conv3(s)))
         s = F.relu(self.bn4(self.conv4(s)))
         s = F.relu(self.bn5(self.conv5(s)))
         s = F.relu(self.bn6(self.conv6(s)))
+        s = F.relu(self.bn7(self.conv6(s)))
 
         s = s.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4))
 
