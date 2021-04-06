5ba6680d3512f84a947a3de6a0dd281fdb83e67d
root <jonathan.tremblay.11@gmail.com>
2021-04-06 03:11:48 +0000

    config

--- configs/config_85pct_new_nn.py
+++ configs/config_85pct_new_nn.py
@@ -7,32 +7,32 @@ import os
 
 config_main = dotdict({
     'numIters': 25,
-    'numEps': 100,  # Number of complete self-play games to simulate during a new iteration.
+    'numEps': 120,  # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,  #
     'updateThreshold': 0.6,
     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,  # Number of game examples to train the neural networks.
     'numMCTSSims': 15,  # Number of games moves for MCTS to simulate.
     'arenaCompare': 20,  # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1,
+    'cpuct': 1.5,
     'nb_of_new_model_for_random_player': 1,
     'nb_of_game_agaisnt_random_player': 50,
 
-    'checkpoint': '/content/drive/MyDrive/AZ_apr5/',
+    'checkpoint': '/content/drive/MyDrive/AZ_apr5/residual/',
     'load_model': False,
-    'load_folder_file': ('/content/drive/MyDrive/AZ_apr5/', 'best.pth.tar'),
+    'load_folder_file': ('/content/drive/MyDrive/AZ_apr5/residual/', 'best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
     'create_log_file': True,
-    'log_file_location': f'/content/drive/MyDrive/AZ_apr5/',
+    'log_file_location': f'/content/drive/MyDrive/AZ_apr5/residual/',
     'log_file_name': f'{os.path.basename(__file__)}_log.txt',
     'log_run_name': f'{os.path.basename(__file__)}'
 })
 config_nn = dotdict({
-    'lr': 0.001,
+    'lr': 0.0011,
     'dropout': 0.25,
     'epochs': 15,
-    'batch_size': 2048,
+    'batch_size': 1024,
     'cuda': torch.cuda.is_available(),
-    'num_channels': 256,
+    'num_channels': 128,
 })
