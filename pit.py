3e36c5dd703b40af1eebcb4d0fb378c471a99682
Jonathan Tremblay <54211599+johntremblay@users.noreply.github.com>
2021-04-25 19:16:18 -0400

    Update pit.py

--- pit.py
+++ pit.py
@@ -15,31 +15,21 @@ any agent.
 
 human_vs_cpu = True
 
+# Instance of the game
 g = SantoriniGame(5, 4)
 
-# all players
+# Players
 rp = RandomPlayer(g).play
 hp = HumanSantoriniPlayer(g).play
 
-# # nnet players
+# # NNet players
 n1 = NNet(g)
 n1.load_checkpoint('./temp/','best.pth.tar')
 args1 = dotdict({'numMCTSSims': 15, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
-#
-# if human_vs_cpu:
-#     player2 = hp
-# else:
-#     n2 = NNet(g)
-#     n2.load_checkpoint('./temp/', 'best.pth.tar')
-#     args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
-#     mcts2 = MCTS(g, n2, args2)
-#     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
-#
-#     player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
 
-# arena = Arena.Arena(rp, rp, g, display=SantoriniGame.display)
+# Create an arena object to let agent play against each other
 arena = Arena.Arena(n1p, rp, g, display=SantoriniGame.display)
 
 print(arena.playGames(1000, verbose=False))
